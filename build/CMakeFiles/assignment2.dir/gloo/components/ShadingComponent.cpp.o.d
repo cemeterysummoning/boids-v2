5b196e18c52fa00af725e9a2ab610f6ebf9a14d0
Ace <yunac477@gmail.com>
2025-11-26 16:19:57 -0500

    Deleted build

--- build/CMakeFiles/assignment2.dir/gloo/components/ShadingComponent.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/assignment2.dir/gloo/components/ShadingComponent.cpp.o: \
- /home/ace/school/2025-2026/6.4400/assignment2/gloo/components/ShadingComponent.cpp \
- /usr/include/stdc-predef.h
